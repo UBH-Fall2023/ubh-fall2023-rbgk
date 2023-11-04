97ceff330f792e435f115c35df148157b6cee09c
SamuelKong <63858847+sskong0719@users.noreply.github.com>
2023-11-04 18:33:23 -0400

    addposter

---
+++ app/src/components/genreData.jsx
@@ -0,0 +1,3 @@
+export const genreData = [
+    "Career", "Graduate", "International", "Health and Wellness", "Guest Speaker", "Politics", "Sports", "Off Campus", "Ticketed Event", "Tutoring", "Service"
+]
