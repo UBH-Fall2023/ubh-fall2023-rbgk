746ae0ed6a4a40d82cf0f06e9e0af79f90e0b2eb
ra341 <razerishi1505@gmail.com>
2023-11-04 18:41:14 -0400

    added SIGN ON

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import Component1 from './components/component1';
 import './App.css';
+import Signin from "./components/auth/components/signin";
 
 function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
+    return (
+        <Router>
+            <AppContent/>
+        </Router>
+    );
 }
 
-function AppContent(){
-  return(
-    <>
-      <Component1 />
-    </>
-  );
+function AppContent() {
+    return <Signin/>
 }
 
 export default App;
