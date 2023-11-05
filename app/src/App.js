23872b7d5473178682445a68cd1819dd2da036ec
ra341 <razerishi1505@gmail.com>
2023-11-05 01:00:02 -0500

    added logout

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage.jsx';
 import './App.css';
 import Signin from "./components/auth/components/signin";
 import AuthDetails from "./components/auth/auth";
+import Signup from "./components/auth/components/signup";
 
 function App() {
     return (
@@ -12,7 +13,7 @@ function App() {
                 <Route path='/' element={<HomePage/>}/>
                 <Route path='/addposter' element={<AddPoster/>}/>
                 <Route path='/signin' element={<Signin/>}/>
-                <Route path='/signup' element={<h1>Signup</h1>}/>
+                <Route path='/signup' element={<Signup/>}/>
                 <Route path='/auth' element={<AuthDetails/>}/>
                 <Route path='*' element={<h1>Not Found</h1>}/>
             </Routes>
